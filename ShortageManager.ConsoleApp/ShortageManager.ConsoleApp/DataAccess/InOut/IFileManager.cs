10e47a9c3b5d8c1765a5c7308432566eef7bd04b
Vytautas Šablinskas <vytsab2@ktu.lt>
2024-06-13 14:40:48 +0300

    Add json file manager

---
+++ ShortageManager.ConsoleApp/ShortageManager.ConsoleApp/DataAccess/InOut/IFileManager.cs
@@ -0,0 +1,8 @@
+﻿namespace ShortageManager.ConsoleApp.DataAccess.InOut;
+
+public interface IFileManager
+{
+    public void Write<T>(string filePath, T data);
+
+    public IEnumerable<T> Read<T>(string filePath);
+}
